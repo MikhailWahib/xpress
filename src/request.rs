82d705ae45fc824e1d0c6c404716520449d218b8
Mikhail <mikhailwahib1@gmail.com>
2024-11-20 12:21:53 +0200

    Feat: add Request::from() method to parse the requests

--- src/request.rs
+++ src/request.rs
@@ -1,4 +1,8 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    io::{BufRead, BufReader, Read},
+    net::TcpStream,
+};
 
 #[derive(Debug)]
 pub struct Request {
@@ -22,14 +26,63 @@ impl Request {
         }
     }
 
-    pub fn from(buffer: &str) -> Self {
-        let mut request = Self::new();
+    pub fn from(buf_reader: &mut BufReader<&TcpStream>) -> Self {
+        let mut request = Request::new();
+        let mut lines = buf_reader.lines();
 
-        let request_line = buffer.split('\n').next().unwrap();
-        let request_lines = request_line.split(' ').collect::<Vec<&str>>();
+        // Parse req line
+        if let Some(Ok(request_line)) = lines.next() {
+            let parts: Vec<&str> = request_line.split_whitespace().collect();
+            if parts.len() == 3 {
+                request.method = parts[0].to_string();
+                request.path = parts[1].to_string();
 
-        request.path = request_lines[1].to_string();
-        request.method = request_lines[0].to_string();
+                // parse query params
+                if let Some((path, query)) = request.path.clone().split_once('?') {
+                    request.path = path.to_string();
+                    request.query = query
+                        .split('&')
+                        .filter_map(|pair| {
+                            let mut kv = pair.split('=');
+                            Some((
+                                kv.next()?.to_string(),
+                                kv.next().unwrap_or_default().to_string(),
+                            ))
+                        })
+                        .collect();
+                }
+            }
+        }
+
+        // Parse headers
+        let mut headers = HashMap::new();
+        for line in lines.by_ref() {
+            let line = match line {
+                Ok(line) => line,
+                Err(_) => break,
+            };
+
+            if line.is_empty() {
+                break;
+            }
+
+            if let Some((key, value)) = line.split_once(": ") {
+                headers.insert(key.to_string(), value.to_string());
+            }
+        }
+        request.headers = headers;
+
+        // Parse body if Content-Length header is present
+        if let Some(content_length) = request
+            .headers
+            .get("Content-Length")
+            .and_then(|cl| cl.parse::<usize>().ok())
+        {
+            let mut body = vec![0; content_length];
+            if buf_reader.read_exact(&mut body).is_ok() {
+                request.body = String::from_utf8_lossy(&body).to_string();
+            }
+        }
 
         request
     }
