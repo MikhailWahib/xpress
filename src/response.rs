7620b2edf85eef3ee900e06e62f5c84813db11d5
Mikhail <mikhailwahib1@gmail.com>
2024-11-29 21:59:34 +0200

    Feat: make Response::json() do the serialization

--- src/response.rs
+++ src/response.rs
@@ -21,23 +21,31 @@ impl Response {
         }
     }
 
-    pub fn status(&mut self, status: u16) {
-        self.status = status;
-    }
-
     pub fn send(&mut self, body: impl Into<Vec<u8>>) -> Result<(), Error> {
         self.body = body.into();
         self.sent = true;
         Ok(())
     }
 
-    pub fn json(&mut self, body: impl Into<Vec<u8>>) -> Result<(), Error> {
+    pub fn status(&mut self, status: u16) {
+        self.status = status;
+    }
+
+    pub fn json<T: serde::Serialize>(&mut self, body: &T) -> Result<(), Error> {
         self.headers
             .insert("Content-Type".to_string(), "application/json".to_string());
 
-        self.body = body.into();
-        self.sent = true;
-        Ok(())
+        match serde_json::to_vec(body) {
+            Ok(serialized) => {
+                self.body = serialized;
+                self.sent = true;
+                Ok(())
+            }
+            Err(_) => Err(Error::new(
+                std::io::ErrorKind::Other,
+                "Serialization failed",
+            )),
+        }
     }
 
     pub fn html(&mut self, body: impl Into<Vec<u8>>) -> Result<(), Error> {
